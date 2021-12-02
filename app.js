4e23e8747365f3b19b37a62a42d1b8b41c9478d2
Vladislav Poliakov <45665990+Luckydog111@users.noreply.github.com>
2021-12-02 14:28:11 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+const subscribe = document.querySelector('.subscribe');
+
+console.log(subscribe);
+
+subscribe.addEventListener('click',event => {
+    
+    alert('Подписка оформлена');
+
+    if (alert) {
+        
+    }
+});
+
